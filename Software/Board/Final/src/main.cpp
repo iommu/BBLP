c4a11a58da84583e054435aa2e6809c84d07d54d
Alex Carter <acarter010204@gmail.com>
2021-09-22 17:28:51 +1000

    Cleaned

--- Software/Board/Final/src/main.cpp
+++ Software/Board/Final/src/main.cpp
@@ -5,9 +5,11 @@
 
 void setup() {
   Serial.begin(115200); // Setup Serial for general logging
-  Wire.begin();   // Setup wire 0 for MUX OLEDs
-  Wire1.begin(16, 17, 100000);  // Setup wire 1 for Primary OLED and Pin Mux
-  Serial.print("1");
+
+  Wire.begin(); // Setup wire 0 for MUX OLEDs
+
+  Wire1.begin(16, 17, 100000); // Setup wire 1 for Primary OLED and Pin Mux
+
   Interface(); // Start primary interface
 }
 
